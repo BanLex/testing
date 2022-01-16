bbbcc07177a3040ee59735d95d1b5b0371c3b118
BanLex <92094117+BanLex@users.noreply.github.com>
2022-01-16 15:23:45 +0300

    Update main.tf

--- yandex-cloud-terraform/main.tf
+++ yandex-cloud-terraform/main.tf
@@ -13,3 +13,28 @@ provider "yandex" {
   folder_id = "${{ YC_FOLDER }}"
   zone      = "ru-central1-a"
 }
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd87va5cc00gaq2f5qfb"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
