3918bbe275a7583d6b254c6cfffa08a3552a12c0
BanLex <92094117+BanLex@users.noreply.github.com>
2022-01-16 17:06:45 +0300

    Update main.tf

--- yandex-cloud-terraform/main.tf
+++ yandex-cloud-terraform/main.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "${{ YC_OAUTH }}"
-  cloud_id  = "${{ YC_CLOUD }}"
-  folder_id = "${{ YC_FOLDER }}"
+  token     = "${ YC_OAUTH }"
+  cloud_id  = "${ YC_CLOUD }"
+  folder_id = "${ YC_FOLDER }"
   zone      = "ru-central1-a"
 }
 
