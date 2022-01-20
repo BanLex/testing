79b4eaa1f92e258ca9b2d91afa30dc0ab8ca8404
BanLex <92094117+BanLex@users.noreply.github.com>
2022-01-20 13:47:19 +0300

    Update dns.tf

--- yandex-cloud-terraform/dns.tf
+++ yandex-cloud-terraform/dns.tf
@@ -2,5 +2,5 @@ resource "yandex_dns_zone" "vpc-peering-zone" {
   name             = "vpc-peering-zone"
   zone             = "mdb.yandexcloud.net."
   public           = false
-  private_networks = [yandex_vpc_network.vm-net.id]
+  private_networks = [yandex_vpc_subnet.subnet-1.id]
 }
