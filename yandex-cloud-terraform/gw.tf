3de46cca89741a617be832d23a183abcb06139d7
BanLex <92094117+BanLex@users.noreply.github.com>
2022-01-23 09:47:28 +0300

    Create gw.tf

---
+++ yandex-cloud-terraform/gw.tf
@@ -0,0 +1,27 @@
+resource "yandex_compute_instance" "gw" {
+  name = "gw"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8drj7lsj7btotd7et5"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("meta.txt")}"
+  }
+
+  provisioner "local-exec" {
+    command = "echo 'HostName ${yandex_compute_instance.gw.network_interface.0.nat_ip_address} \\nUser banlex' >> ../ops/ssh-config"
+  }
+}
