e70777c463f6436c8793cdb529845bf5eab76ef1
BanLex <92094117+BanLex@users.noreply.github.com>
2022-01-23 09:45:53 +0300

    Create docker.tf

---
+++ yandex-cloud-terraform/docker.tf
@@ -0,0 +1,24 @@
+resource "yandex_compute_instance" "docker" {
+  count = local.countss
+  name = "docker-${count.index}"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8f30hur3255mjfi3hq"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = false
+  }
+
+  metadata = {
+    user-data = "${file("meta.txt")}"
+  }
+}
