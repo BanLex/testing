3793f8a38003f13f070f034f117ec25a8b54ba01
BanLex <92094117+BanLex@users.noreply.github.com>
2022-01-24 19:40:38 +0300

    Update network.tf

--- yandex-cloud-terraform/network.tf
+++ yandex-cloud-terraform/network.tf
@@ -13,7 +13,7 @@ resource "yandex_vpc_route_table" "net-rt-a" {
   network_id = "${yandex_vpc_network.network-1.id}"
 
   static_route {
-    destination_prefix = "0.0.0.0"
+    destination_prefix = "0.0.0.0/0"
     next_hop_address   = "192.168.10.3"
   }
 }
