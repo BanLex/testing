14f78f79a920a1f1805763b556bf14ff53b7b343
BanLex <92094117+BanLex@users.noreply.github.com>
2022-01-24 19:55:25 +0300

    Update network.tf

--- yandex-cloud-terraform/network.tf
+++ yandex-cloud-terraform/network.tf
@@ -7,6 +7,7 @@ resource "yandex_vpc_subnet" "subnet-1" {
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
+  route_table_id = "${yandex_vpc_route_table.net-rt-a.id}"
 }
 
 resource "yandex_vpc_route_table" "net-rt-a" {
