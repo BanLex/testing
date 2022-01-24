15fd99030d3e040f408e4952ca25c0411fe923f1
BanLex <92094117+BanLex@users.noreply.github.com>
2022-01-24 13:57:25 +0300

    Update provider.tf

--- yandex-cloud-terraform/provider.tf
+++ yandex-cloud-terraform/provider.tf
@@ -1,11 +1,29 @@
+locals {
+  countss = 4
+}
 
+terraform {
+  backend "s3" {}
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+data "terraform_remote_state" "s3" {
+  backend = "s3"
+  config {    
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "banlex.terraform"
+    region     = "us-east-1"
+    key        = "tester.tfstate"
+    access_key = "${local.access_key}"
+    secret_key = "${local.secret_key}"
     skip_region_validation      = true
     skip_credentials_validation = true
   }
 }
-
-
-
 provider "yandex" {
   token     = "${ var.oauth }"
   cloud_id  = "${ var.cloud }"
